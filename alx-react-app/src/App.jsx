f6e08a52597fc396fafd692e604a5c4b74a136f2
yuslove1 <adexyuslove@gmail.com>
2024-08-11 01:59:37 -0700

    Modify JSX in a Pre-built React Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      < WelcomeMessage />
     </>
   )
 }
 
-export default App
+export default App;
