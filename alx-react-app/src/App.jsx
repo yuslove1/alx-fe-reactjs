a520f504ca04e6919d394d3ad4aa93c4478810a7
yuslove1 <adexyuslove@gmail.com>
2024-08-11 02:02:36 -0700

    Modify JSX in a Pre-built React Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      < WelcomeMessage />
+      <WelcomeMessage />
     </>
   )
 }
