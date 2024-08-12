2b4447d21068e3a3794672a00b87c1385ae4776c
yuslove1 <adexyuslove@gmail.com>
2024-08-12 02:16:05 -0700

    style the footer

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return(
-        <footer>
-        <p>© 2023 City Lovers</p>
+        <footer style={{ backgroundColor: '#d6ebff' }}>
+        <p style={{ color: '#969CA2' }}>© 2023 City Lovers</p>
       </footer> 
     );
 }
