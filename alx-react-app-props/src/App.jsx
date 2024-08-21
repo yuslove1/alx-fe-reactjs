21597a6cfceeb168c43af3da55c49b578a949ef2
yuslove1 <adexyuslove@gmail.com>
2024-08-21 09:49:22 -0700

    Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css'
 import ProfilePage from './components/ProfilePage';
 import UserContext from './components/UserContext';
+import UserProfile from './components/UserProfile';
 
   function App() {
-    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com", age :"25", bio:"Loves hiking and photography" };
   
     return (
       <UserContext.Provider value={userData}>
-      <ProfilePage userData={userData} />
+        <div>
+        <ProfilePage userData={userData} />
+
+        <UserProfile userData={userData} />
+        </div>
       </UserContext.Provider>
     )
 
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-const UserProfile = (props) =>{
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = () =>{
+    const user = React.useContext(UserContext);
+
     return(
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
+            <h2>{user.name}</h2>
+            <p>Age: {user.age}</p>
+            <p>Bio: {user.bio}</p>
         </div>
     );
 };
