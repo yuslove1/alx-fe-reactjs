b63d626aa33509c1010acd4c9f34c493383ea179
yuslove1 <adexyuslove@gmail.com>
2024-09-13 07:14:05 -0700

    fix: importation of UserProfile

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import UserProfile from './components/UserProfile';
 
 function App() {
 
   return (
-    <div className="text-blue-400">
+    <>
+     <div className="text-blue-400">
       <h1 >wellcome to react 2.0</h1>
     </div>
+    <UserProfile />
+    </>
+   
   )
+
+  
 }
 
 export default App;
