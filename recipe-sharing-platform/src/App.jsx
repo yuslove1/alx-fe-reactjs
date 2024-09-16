7ac71617f4312ba5f82ae74c7f37d6337337fd87
yuslove1 <adexyuslove@gmail.com>
2024-09-16 06:29:06 -0700

    setup the React Router

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,22 @@
+import HomePage from './components/HomePage'
+import RecipeDetail from './components/RecipeDetail';
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
 
   return (
     <>
-  <h1>Hello, welcome to tailwind project 2</h1>
+    <Router>
+    <h1>Hello, welcome to tailwind project 2</h1>
+    <Routes>
+    <Route path='/' element={<HomePage />} />
+    <Route path='/recipe/:id' element={<RecipeDetail />} />
+
+    </Routes>
+    </Router>
     </>
   )
 }
