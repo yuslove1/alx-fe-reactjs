6459b1ba6f4322df1ba586a9bffccf4cfb737202
yuslove1 <adexyuslove@gmail.com>
2024-09-16 07:23:22 -0700

    feat: the Recipe Detail Page

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -13,21 +13,30 @@ function RecipeDetail() {
       .then((datas) => {
         const recipe = datas.find((data) => data.id === parseInt(id)); //find the exact recipe from recipes
         setRecipes(recipe);
-        console.log(recipe)
       });
   }, [id]);
 
   return (
     <div className="">
-      <div className="mx-auto my-20 sm:max-w-sm lg:max-w-xl bg-slate-200 flex flex-col items-center shadow-lg">
+      <div className="mx-auto my-20 sm:max-w-md lg:max-w-xl bg-slate-200 shadow-lg p-5">
       {/* image container */}
         <div className="my-8"> 
-          <img className="rounded-lg" src={recipe.image} alt="" />
+          <img className="rounded-lg mx-auto" src={recipe.image} alt="" />
         </div>
 
-        <h1 className="text-2xl text-red-800 hover:text-red-400">{recipe.title}</h1>
+        <h1 className="text-2xl text-red-800 hover:text-red-400 font-extrabold">{recipe.title}</h1>
         <p className="my-10 text-balance text-center px-5">{recipe.summary}</p>
+
+        <div>
+        <h3 className="text-red-800 font-extralight">INGREDIENTS: </h3>
+        {recipe.ingredients? <p className="mb-5 text-red-950">{recipe.ingredients}</p> : 'ingredient is not availaible.......'}
+       
+       
+        <h3 className="text-red-800 mt-3 font-extralight">STEPS: </h3>
+        {recipe.steps? <p className="mb-5 text-red-950">{recipe.steps}</p> : <p className="font-thin">steps is not availaible.......</p>}
       </div>
+      </div>
+      
     </div>
   );
 }
