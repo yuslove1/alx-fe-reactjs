6b5f79bb72900361f4eea6fa28e240c1f9f35d1f
yuslove1 <adexyuslove@gmail.com>
2024-09-16 07:35:16 -0700

    feat: the Recipe Detail Page

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -24,16 +24,16 @@ function RecipeDetail() {
           <img className="rounded-lg mx-auto" src={recipe.image} alt="" />
         </div>
 
-        <h1 className="text-2xl text-red-800 hover:text-red-400 font-extrabold">{recipe.title}</h1>
+        <h1 className="text-2xl text-red-800 hover:text-red-400 font-extrabold text-center">{recipe.title}</h1>
         <p className="my-10 text-balance text-center px-5">{recipe.summary}</p>
 
         <div>
-        <h3 className="text-red-800 font-extralight">INGREDIENTS: </h3>
-        {recipe.ingredients? <p className="mb-5 text-red-950">{recipe.ingredients}</p> : 'ingredient is not availaible.......'}
+        <h3 className="text-red-800 font-bold">INGREDIENTS: </h3>
+        {recipe.ingredients? <p className="mb-5 text-red-950">{recipe.ingredients}</p> :  <p className="font-thin italic text-sm">no ingredients availaible.......</p>}
        
        
-        <h3 className="text-red-800 mt-3 font-extralight">STEPS: </h3>
-        {recipe.steps? <p className="mb-5 text-red-950">{recipe.steps}</p> : <p className="font-thin">steps is not availaible.......</p>}
+        <h3 className="text-red-800 mt-3 font-bold">COOKING INSTRUCTIONS </h3>
+        {recipe.instructions? <p className="mb-5 text-red-950">{recipe.instructions}</p> : <p className="font-thin italic text-sm">no cooking instructions availaible.......</p>}
       </div>
       </div>
       
