305e76f3821fecc5758db755a04dda2e57473dee
yuslove1 <adexyuslove@gmail.com>
2024-09-17 09:15:46 -0700

    Implementing a Responsive Form to Add New Recipes

---
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+
+function AddRecipeForm() {
+  const [formData, setFormData] = useState({
+    title: "",
+    ingredients: "",
+    instructions: "",
+    errorMessage: {
+      title: "",
+      ingredients: "",
+      instructions: "",
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validation
+    const errors = {};
+
+    if (!formData.title.trim()) errors.title = "Please enter title";
+    if (!formData.ingredients.trim())
+      errors.ingredients = "Please enter ingredients";
+    if (!formData.instructions.trim())
+      errors.instructions = "Please enter instruction";
+
+    if (Object.keys(errors).length > 0) {
+      setFormData({ ...formData, errorMessage: errors });
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+      errorMessage: { ...formData.errorMessage, [e.target.name]: "" },
+    });
+  };
+
+  return (
+    <div className="container mx-auto my-20">
+      {/* <div className=" w-1/2 bg-purple-100 mx-auto rounded-lg p-6 shadow-md" ></div> */}
+
+      <h1 className="text-center text-3xl font-bold text-gray-800 font-mono "> ADD NEW RECIPE</h1>
+      <form className="max-w-2xl mx-auto shadow-lg lg:p-10 p-2 rounded-sm" onSubmit={handleSubmit}>
+        
+        <div className="mb-5">
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            placeholder="Enter the Title"
+            className="outline-double w-full bg-transparent p-2 rounded-md"
+          />
+          {formData.errorMessage.title ? (
+            <p>{formData.errorMessage.title}</p>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="mb-5">
+          <textarea
+            name="ingredients"
+            id=""
+            value={formData.ingredients}
+            onChange={handleChange}
+            placeholder="List of ingredient"
+            className="outline-double w-full h-24  bg-transparent p-2 rounded-md"
+          ></textarea>
+          {formData.errorMessage.ingredients ? (
+            <p>{formData.errorMessage.ingredients}</p>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div className="mb-8">
+          <textarea
+            name="instructions"
+            id=""
+            value={formData.instructions}
+            onChange={handleChange}
+            placeholder="The cooking instructions"
+            className="outline-double w-full h-24  bg-transparent p-2 rounded-md"
+          ></textarea>
+          {formData.errorMessage.instructions ? (
+            <p>{formData.errorMessage.instructions}</p>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow block mx-auto " type="submit">
+          Submit
+        </button>
+      </form>
+      {/* <hr />
+      <div>
+        <h1>error message</h1>
+        <p>{formData.errorMessage.title}</p>
+      </div> */}
+    </div>
+  );
+}
+
+export default AddRecipeForm;
