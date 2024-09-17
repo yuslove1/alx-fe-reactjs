49bae8a043366c4fdb950ceb7317f2b40ba8dacc
yuslove1 <adexyuslove@gmail.com>
2024-09-17 10:12:14 -0700

    Implementing a Responsive Form to Add New Recipes

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -9,39 +9,56 @@ function AddRecipeForm() {
   const [errors, setErrors] = useState({
     title: "",
     ingredients: "",
-    steps: ""
-  })
+    steps: "",
+  });
 
+  //Validation function
+  const validate = () => {
+    const newError = {};
+
+    if (!formData.title.trim()) newError.title = "Please enter title";
+    if (!formData.ingredients.trim())
+      newError.ingredients = "Please enter ingredients";
+    if (!formData.steps.trim()) newError.steps = "Please enter cooking steps";
+
+    return newError;
+  };
+
+  // handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    //Validation
-    const err = {};
-
-    if (!formData.title.trim()) err.title = "Please enter title";
-    if (!formData.ingredients.trim()) err.ingredients = "Please enter ingredients";
-    if (!formData.steps.trim()) err.steps = "Please enter cooking steps";
-
+    //call validate and if there is any error in the error object
+    //and  pass it to the errors state
+    const err = validate();
     if (Object.keys(err).length > 0) {
       setErrors(err);
     }
+
   };
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-    setErrors({...errors, [e.target.name] : "" })
+
+    //disparse the erroMessage once user started entering on the value
+    setErrors({ ...errors, [e.target.name]: "" });
   };
 
   return (
     <div className="container mx-auto my-20">
       {/* <div className=" w-1/2 bg-purple-100 mx-auto rounded-lg p-6 shadow-md" ></div> */}
 
-      <h1 className="text-center text-3xl font-bold text-gray-800 font-mono "> ADD NEW RECIPE</h1>
-      <form className="max-w-2xl mx-auto shadow-lg lg:p-10 p-2 rounded-sm" onSubmit={handleSubmit}>
-        
+      <h1 className="text-center text-3xl font-bold text-gray-800 font-mono ">
+        {" "}
+        ADD NEW RECIPE
+      </h1>
+      <form
+        className="max-w-2xl mx-auto shadow-lg lg:p-10 p-2 rounded-sm"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-5">
           <input
             type="text"
@@ -51,11 +68,7 @@ function AddRecipeForm() {
             placeholder="Enter the Title"
             className="outline-double w-full bg-transparent p-2 rounded-md"
           />
-          {errors.title ? (
-            <p className="text-red-500">{errors.title}</p>
-          ) : (
-            ""
-          )}
+          {errors.title ? <p className="text-red-500">{errors.title}</p> : ""}
         </div>
         <div className="mb-5">
           <textarea
@@ -82,14 +95,13 @@ function AddRecipeForm() {
             placeholder="The cooking steps"
             className="outline-double w-full h-24  bg-transparent p-2 rounded-md"
           ></textarea>
-          {errors.steps ? (
-            <p className="text-red-500">{errors.steps}</p>
-          ) : (
-            ""
-          )}
+          {errors.steps ? <p className="text-red-500">{errors.steps}</p> : ""}
         </div>
 
-        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow block mx-auto " type="submit">
+        <button
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow block mx-auto "
+          type="submit"
+        >
           Submit
         </button>
       </form>
@@ -101,5 +113,4 @@ function AddRecipeForm() {
     </div>
   );
 }
-
 export default AddRecipeForm;
