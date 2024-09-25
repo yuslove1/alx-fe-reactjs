2d8a928f93de2d1db7d1f057c6728d878e047739
yuslove1 <adexyuslove@gmail.com>
2024-09-25 09:33:39 -0700

    fix:

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
+import ReactDOM from 'react-dom/client'
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Layout from './components/Layout'
+import AdvanceSearch from './components/AdvanceSearch'
 import Search from './components/Search'
 
 function App() {
   return (
-    <>
-   <h1>Search Github Username here</h1>
-   <Search />
-    </>
+//  <Router>
+//   <Routes>
+//     <Route path='/' element={<Layout />}>
+//     <Route index element={<Search />}/>
+//     <Route path='advance' element={<AdvanceSearch />}/>
+//     </Route>
+//   </Routes>
+//  </Router>
+<div>
+  <Search />
+</div>
   )
 }
 
-export default App
+export default App;
