da67384c70669f64f4dd137a748b528006c9e42f
yuslove1 <adexyuslove@gmail.com>
2024-09-25 09:30:36 -0700

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -3,61 +3,125 @@ import { fetchUserData } from "../services/githubService";
 
 function Search() {
   const [username, setUsername] = useState("");
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [location, setLocation] = useState("");
+  const [minRep, setMinRep] = useState("");
+  const [page, setPage] = useState(1);
 
-  const handleSearch = (e) => {
-e.preventDefault();
-    setIsLoading(true)
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
     setIsError(false);
+    setUserData([]);
+    setUsername("");
+    setLocation("");
+    setMinRep("");
 
-    fetchUserData(username)
-      .then((data) => {
-        setIsLoading(false);
-        setUserData(data);
-        // console.log(data);
-      })
-      .catch((error) => {
-        setIsError(true);
-        setIsLoading(false);
-        // console.error("Error fetching data", error)
-      });
+    // fetchUserData(username, location, minRep)
+    //   .then((data) => {
+    //     setIsLoading(false);
+    //     setUserData(data.items);
+    //   })
+    //   .catch((error) => {
+    //     setIsError(true);
+    //     setIsLoading(false);
+    //     console.error("Error fetching data", error)
+    //   });
 
-      setUsername('')
+    const data = await fetchUserData(username, location, minRep, page);
+
+    if (data && data.items.length > 0) {
+      setIsLoading(false);
+      setUserData(data.items);
+      setPage(page + 1);
+    } else {
+      setIsError(true);
+      setIsLoading(false);
+    }
   };
 
   return (
-    <div>
-        <form onSubmit={handleSearch}>
-        <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter github username"
-      />
+    <>
+      <div className="container mx-auto px-4">
+        <h1 className="my-8 text-3xl text-center text-slate-600 font-bold">
+          GItHUB PROFILE SEARCHER
+        </h1>
 
-      <button type="submit">Search</button>
-        </form>
-      
-     
-      <hr />
+        <div id="form-cotainer" className="max-w-2xl mx-auto shadow-lg">
+          <form onSubmit={handleSearch}>
+            <div className=" pt-2 items-center">
+              <label
+                htmlFor="input-1"
+                className="block text-sm font-medium text-gray-900 mb-2"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter github username"
+                id="input-1"
+                className="bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full ps-10 p-2.5 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:shadow-lg"
+              />
+            </div>
 
-      <div>
+            <h2 className="text-center font-semibold my-4">Advance Search</h2>
+            <div className="w-full flex gap-4 justify-center">
+              <input
+                type="text"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                placeholder="Enter location"
+                className="bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-10/12 ps-10 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:shadow-lg"
+              />
+              <input
+                type="number"
+                value={minRep}
+                onChange={(e) => setMinRep(e.target.value)}
+                placeholder="Minimum Repos"
+                className="bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-3/12 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:shadow-lg"
+              />
+            </div>
+            <button
+              className="p-4 rounded-md bg-slate-500 self-center mt-5 mx-auto block text-white hover:bg-slate-700"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+      <hr className="shadow-2xl" />
+
+      <div className="max-w-2xl mx-auto">
         {isLoading && <p>Loading......</p>}
         {isError && <p>Looks like we cant find the user</p>}
-        {userData && (
-          <div>
-            <div>
-              <img src={userData.avatar_url} alt="avater" />
-            </div>
-            <h1>{userData.name}</h1>
-            <h2>{userData.login}</h2>
-            <a href={userData.html_url}>{userData.html_url}</a>
-          </div>
-        )}
+        <div className="container grid grid-cols-3 gap-3 mx-auto mt-6">
+          {Array.isArray(userData) &&
+            userData.map((user) => (
+              <div key={user.id} className="flex justify-center">
+                <div>
+                  <img src={user.avatar_url} alt="avater" />
+                </div>
+
+                <div className="p-2">
+                  <h1 className="bg-gray-300 font-medium font-mono rounded">{user.login}</h1>
+                  <h2>User Login:{user.location}</h2>
+                  <p>Repositories :{user.public_repos}</p>
+                  <a href={user.html_url} className="text-blue-500">view profile</a>
+                </div>
+              </div>
+            ))}
+        </div>
+
+        {/* <button onClick={handleSearch} disabled={isLoading}>
+          {isLoading ? "Loading..." : "Load More"}
+        </button> */}
       </div>
-    </div>
+    </>
   );
 }
 
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,47 @@
-import axios from 'axios'
+// import axios from "axios";
 
-export function fetchUserData (username) {
-   return axios.get(`https://api.github.com/users/${username}`)
-    .then((response) => response.data)
-    .catch((error) => {throw error});
-}
+// export function fetchUserData(user, loc, repNum) {
+//   let query = `username:${user}`
+
+//   return axios
+//     .get(`https://api.github.com/users/q=${query}`)
+//     .then((response) => response.data)
+//     .catch((error) => {
+//       throw error;
+//     });
+// }
+
+// export function fiterUsers(location, repNum) {
+//   return axios
+//     .get(
+//       `https://api.github.com/search/users?q=location:${location}+repos:>${repNum}`
+//     )
+//     .then((response) => response.data)
+//     .catch((error) => {
+//       throw error;
+//     });
+// }
+
+import axios from "axios";
+
+export const fetchUserData = async (username, location, minRepoCount, page) => {
+  try {
+    let query = username ? `${username} in:login` : "";
+
+    if (location) {
+      query += ` location:${location}`;
+    }
+
+    if (minRepoCount) {
+      query += ` repos:>${minRepoCount}`;
+    }
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${query}&page=${page}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("error fetching data:", error);
+    return null;
+  }
+};
